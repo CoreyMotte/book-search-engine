3a52eb60c846078cd0844e1e994471a909156fe0
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-07-06 10:20:31 -0400

    started resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,10 +6,35 @@ const resolvers = {
     Query: {
         me: async(parent, args, context) => {
             if (context.user) {
-                const user = await User.findOne({_id: context.user._id}).select('-__v -password');
+                const user = await User.findOne({_id: context.user._id});
                 return user;
             }
             throw new AuthenticationError('You must be logged in!')
+        }
+    },
+
+    Mutation: {
+        addUser: async(parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return {token, user};
+        },
+        login: async(parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            console.log(user)
+
+            if (!user) {
+                throw new AuthenticationError('User not found');
+            }
+
+            const correctPassword = await user.isCorrectPassword(password);
+            if (!correctPassword) {
+                throw new AuthenticationError('Password incorrect!')
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
         }
     }
 };
