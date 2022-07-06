1cfe195fddd37ce68a099f3cc75f33cc71142715
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-07-06 09:39:01 -0400

    set up apolloserver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,7 +3,15 @@ const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-
+    Query: {
+        me: async(parent, args, context) => {
+            if (context.user) {
+                const user = await User.findOne({_id: context.user._id}).select('-__v -password');
+                return user;
+            }
+            throw new AuthenticationError('You must be logged in!')
+        }
+    }
 };
 
 module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -4,9 +4,27 @@ const db = require('./config/connection');
 const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const { type } = require('os');
+const { start } = require('repl');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    console.log(`GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,8 +34,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+startApolloServer(typeDefs, resolvers);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+
